e5455af908b9f674ac4e8550f8e36024181b7ee1
Kavya196 <57001643+Kavya196@users.noreply.github.com>
2020-02-28 18:59:31 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+#define touchSw 4
+#define led 10
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mlx.begin();
+    pinMode(touchSw,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.println("Temperature from MLX90614:");
+  Serial.print("Ambient:      ");
+  Serial.print(mlx.readAmbientTempC());
+  Serial.println(" °C");
+  Serial.print("Contactless: ");
+  Serial.print(mlx.readObjectTempC());
+  Serial.println(" °C");
+  Serial.println();
+//  delay(500);
+   boolean touchState;
+  touchState = digitalRead(touchSw);
+
+  if(touchState == HIGH)
+  {
+    digitalWrite(led, HIGH);//write statements to execute when the sw is high
+  }
+  else
+  {
+    digitalWrite(led,LOW);//write statements to execute when the sw is low
+  }
+ //   delay(500);
+//  Serial.println(touchState);
 }
