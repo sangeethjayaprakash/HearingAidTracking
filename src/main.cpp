a4c5d14fa4b7400913020be52e6cb07d8213a560
Kavya196 <57001643+Kavya196@users.noreply.github.com>
2020-03-03 15:35:47 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <SoftwareSerial.h>
-#include <Wire.h>
 
-#define TOUCH 4
-#define LED 10
-#define NORMAL_BODY_TEMP 38
-#define BT_TX 2
-#define BT_RX 3
-#define ADDR 0x68
+
+#define NORMAL_BODY_TEMP 25
 #define TH_ACCEL 200
 
+#define TOUCH 8
+
+#define R_LED1 A0
+#define G_LED1 A1
+#define R_LED2 A2
+#define G_LED2 A3
+
+#define BT_RX 2
+#define BT_TX 3
+
+#define ADDR 0x68
+
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-SoftwareSerial bt(BT_TX, BT_RX);
+SoftwareSerial bt(BT_RX, BT_TX);
 
 boolean touchState = LOW;
 float temperature = 0;
@@ -22,6 +29,14 @@ String alarm_status = "\0";
 unsigned int data[6];
 
 void setup() {
+  pinMode(TOUCH, INPUT);
+  pinMode(R_LED1, OUTPUT);
+  pinMode(G_LED1, OUTPUT);
+  pinMode(R_LED2, OUTPUT);
+  pinMode(G_LED2, OUTPUT);
+  Serial.begin(115200);
+  bt.begin(9600);
+  mlx.begin();
   Wire.begin();
   
   // Start I2C transmission
@@ -50,43 +65,62 @@ void setup() {
   Wire.write(0x01);
   // Stop I2C transmission
   Wire.endTransmission();
-  
-  Serial.begin(115200);
-  bt.begin(9600);
-  mlx.begin();
-  pinMode(TOUCH,INPUT);
 }
 
 // Tracking function
+// Add RGB Led codes here
 void trackUsage(boolean touchState, float temperature, int acceleration) {
-  if (touchState == HIGH && temperature > NORMAL_BODY_TEMP && acceleration > TH_ACCEL) {
+  if (touchState == HIGH && temperature >NORMAL_BODY_TEMP &&acceleration > TH_ACCEL) {
     Serial.println("Device worn");
-    bt.println("ON");
+    analogWrite(R_LED1, 0);
+    analogWrite(R_LED2, 0);
+    bt.println("ONN");
   }
   else {
     Serial.println("Device taken off");
+    analogWrite(R_LED1, 1024);
+    analogWrite(R_LED2, 1024);
     bt.println("OFF");
   }
 }
 
 // Blink alarm function
-void blinkAlarm() {
-  digitalWrite(LED, HIGH);
+void blinkAlarm(boolean state) {
+  if (state) {
+     analogWrite(G_LED1, HIGH);
+   analogWrite(G_LED2, HIGH);
+    Serial.println("TRU");
+  }
+  else {
+     analogWrite(G_LED1, LOW);
+     analogWrite(G_LED2, LOW);
+   Serial.println("FLS");
+  }
 }
 
 void loop() {
-  while (bt.available()){
-    c = bt.read();
-    if (c == '#') {
-      break;
+  if (bt.available()) {
+    alarm_status = "\0";
+    while (bt.available()){
+      c = bt.read();
+      if (c == '#') {
+        break;
+      }
+      alarm_status += c;
     }
-    alarm_status += c;
+    alarm_status.trim();
+    Serial.println(alarm_status);
+    
+    if (alarm_status == "TRU") {
+      blinkAlarm(true);
+    }
+    
+//    if (alarm_status == "FLS") {
+//      blinkAlarm(false);
+//    }
   }
-  alarm_status.trim();
-  if (alarm_status == "TRUE") {
-    blinkAlarm();
-  }
-  
+   touchState = digitalRead(TOUCH);
+  temperature = (float)mlx.readObjectTempC();
   // Start I2C transmission
   Wire.beginTransmission(ADDR);
   // Select data register
@@ -114,7 +148,9 @@ void loop() {
   int zAccl = data[4] * 256 + data[5];
   int acceleration = sqrt(pow(xAccl, 2) + pow(yAccl, 2) + pow(zAccl, 2));
   
-  touchState = digitalRead(TOUCH);
-  temperature = (float)mlx.readObjectTempC();
+ Serial.println(acceleration);   
+ Serial.println(touchState);
+ Serial.println(temperature); 
   trackUsage(touchState, temperature, acceleration);
+  delay(2000);
 }
